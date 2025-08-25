219e9cb1063842b440fd83d362987ab69e2182b2
TheNewRobot <afloresa@ucla.edu>
2025-08-25 13:36:57 -0500

    Created a basic environment and a placeholder

---
+++ q8bot_sim/mujoco/src/envs/basic_env.py
@@ -0,0 +1,13 @@
+import numpy
+
+from mujoco_playground._src import mjx_env
+from mujoco_playground._src.locomotion.spot.base import SpotEnv
+
+class Q8BotEnv(mjx_env.MjxEnv):
+    """Tiny quadruped environment class"""
+    pass
+
+class SpotPlaceholderEnv(SpotEnv):
+    """Placeholder class for Q8BotEnv"""
+    pass
+
